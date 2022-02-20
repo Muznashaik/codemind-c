8f685109deb022ac4555bd8172cdd292656c3707
muznashaik <97612575+Muznashaik@users.noreply.github.com>
2022-02-20 23:09:12 +0530

    #include<stdio.h> int main() {   int n,a=0,b=1,c,i;    scanf("%d",&n);    for(i=1;i<=n;i++)    {        c=a+b;        if(c==n)        {            printf("True");            break;        }        if(c>n)        {            printf("False");            break;        }        a=b;        b=c;    }    return 0; }

---
+++ Fibonacci_or_not.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+  int n,a=0,b=1,c,i;
+   scanf("%d",&n);
+   for(i=1;i<=n;i++)
+   {
+       c=a+b;
+       if(c==n)
+       {
+           printf("True");
+           break;
+       }
+       if(c>n)
+       {
+           printf("False");
+           break;
+       }
+       a=b;
+       b=c;
+   }
+   return 0;
+}
