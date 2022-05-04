e415c0ea45ccd5fe60aa745d45306a4cafa701c6
muznashaik <97612575+Muznashaik@users.noreply.github.com>
2022-05-04 22:19:02 +0530

    #include<stdio.h> int main() {     int n,arr[100],i,c=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=1;i<n;i++)     {         if(arr[i-1]%2!=0 && arr[i+1]%2!=0)         {             c++;         }     }     printf("%d",c);     return 0; }

---
+++ count_even_numbers_between_odd_numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,arr[100],i,c=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=1;i<n;i++)
+    {
+        if(arr[i-1]%2!=0 && arr[i+1]%2!=0)
+        {
+            c++;
+        }
+    }
+    printf("%d",c);
+    return 0;
+}
