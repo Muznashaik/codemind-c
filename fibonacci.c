b9d50cc40d897fc4550ab0a19f282da7ffedab7e
muznashaik <97612575+Muznashaik@users.noreply.github.com>
2022-05-04 17:54:11 +0530

    #include<stdio.h> int main() {     int n,i,a=0,b=1,c;     scanf("%d",&n);     printf("%d ",a);     printf("%d ",b);     for(i=0;i<n-2;i++)     {         c=a+b;         a=b;         b=c;         printf("%d ",c);     }     return 0; }

--- fibonacci.c
+++ fibonacci.c
@@ -1,14 +1,16 @@
 #include<stdio.h>
 int main()
 {
-  int n,a=0,b=1,c,i;
-   scanf("%d",&n);
-   for(i=1;i<=n;i++)
-   {
-       printf("%d ",a);
-       c=a+b;
-       a=b;
-       b=c;
-   }
-   return 0;
+    int n,i,a=0,b=1,c;
+    scanf("%d",&n);
+    printf("%d ",a);
+    printf("%d ",b);
+    for(i=0;i<n-2;i++)
+    {
+        c=a+b;
+        a=b;
+        b=c;
+        printf("%d ",c);
+    }
+    return 0;
 }
