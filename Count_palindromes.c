9926c72afd35c190c0cdbebed4a2f6c79e9544b9
muznashaik <97612575+Muznashaik@users.noreply.github.com>
2022-05-29 12:49:05 +0530

    #include<stdio.h> int main() {     int n,arr[100],i,temp,s=0,c=0,d;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         temp=arr[i];         s=0;         while(arr[i]!=0)         {             d=arr[i]%10;             arr[i]=arr[i]/10;             s=s*10+d;         }         if(s==temp)         {             c++;         }     }     printf("%d ",c);     return 0; }

---
+++ Count_palindromes.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int n,arr[100],i,temp,s=0,c=0,d;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        temp=arr[i];
+        s=0;
+        while(arr[i]!=0)
+        {
+            d=arr[i]%10;
+            arr[i]=arr[i]/10;
+            s=s*10+d;
+        }
+        if(s==temp)
+        {
+            c++;
+        }
+    }
+    printf("%d ",c);
+    return 0;
+}
