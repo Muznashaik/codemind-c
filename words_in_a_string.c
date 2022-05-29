1daa974772e385f59b49deeeb1b4246dd23eebe5
muznashaik <97612575+Muznashaik@users.noreply.github.com>
2022-05-29 13:05:08 +0530

    #include<stdio.h> int main() {     char str[100];     int i,c=0;     scanf("%[^ ]s",str);     for(i=0;str[i]!=NULL;i++)     {         if(str[i]==' ')         {             c++;         }     }     printf("%d",c+1); }

---
+++ words_in_a_string.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    char str[100];
+    int i,c=0;
+    scanf("%[^
+]s",str);
+    for(i=0;str[i]!=NULL;i++)
+    {
+        if(str[i]==' ')
+        {
+            c++;
+        }
+    }
+    printf("%d",c+1);
+}
