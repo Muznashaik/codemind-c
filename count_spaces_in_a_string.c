1637c6fee9d7dd23d1350f61f0cf1f191c03dd0d
muznashaik <97612575+Muznashaik@users.noreply.github.com>
2022-05-31 11:02:22 +0530

    #include<stdio.h> int main() {     char str[100];     int i,j,c=0;     scanf("%[^ ]s",str);     for(i=0;str[i]!=NULL;i++)     {         if(str[i]==' ')         {             c++;         }     }     printf("%d",c);     return 0; }

---
+++ count_spaces_in_a_string.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    char str[100];
+    int i,j,c=0;
+    scanf("%[^
+]s",str);
+    for(i=0;str[i]!=NULL;i++)
+    {
+        if(str[i]==' ')
+        {
+            c++;
+        }
+    }
+    printf("%d",c);
+    return 0;
+}
